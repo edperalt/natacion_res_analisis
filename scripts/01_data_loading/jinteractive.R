98ef04ff236ffa02e3ebd608edb0cd958553d233
edperalt <edperalt@gmail.com>
2021-04-12 23:22:35 +0200

    data loading ready tried the interactive pdf reading, very cool started some basic plotting, more customization needed.

---
+++ scripts/01_data_loading/jinteractive.R
@@ -0,0 +1,12 @@
+library(tabulizer)
+
+
+
+
+f <- system.file( "lattice-timelog-multiple-pages.pdf",package = "tabulizer")
+
+
+test<- extract_areas("raw_data/resultados_completos_master_invierno_2021.pdf", pages = )
+
+
+test[2]
